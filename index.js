8811c046d20d5ec813c04f1aea7586b44ea6e286
IronKyle38 <63471198+IronKyle38@users.noreply.github.com>
2023-04-04 21:18:32 +0200

    Save prices in a .csv output file

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 // Install puppeteer: npm install puppeteer
 // Run: node index.js
 
-// Import puppeteer
+// Import modules
+const fs = require('fs');
 const { getPrice } = require('./functions/getPrice');
 const puppeteer = require('puppeteer');
 
 // Open settings file
 const settings = require('./input/settings.json');
 
+// Create today's date
+const today = new Date();
+const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
 // Loop through the products
 for (let n = 0; n < settings.products.length; n++) {
   // Get the price
-  getPrice(settings, n);
+  getPrice(settings, n).then(price => {
+    // Save the price to an output file .csv
+    fs.appendFile(`./output/${settings.products[n].productName}.csv`, `${date},${settings.products[n].siteName},${settings.products[n].productQuantity},${price}\n`, err => {
+      if (err) throw err;
+    });
+  });
 }
